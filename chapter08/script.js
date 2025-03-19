37a2de945dbd78903296b1e36b75fb188bbcd66b
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-03-19 14:18:19 +0500

    incomplete!

---
+++ chapter08/script.js
@@ -0,0 +1,122 @@
+var products = [
+    {
+      id: 101,
+      title: "Sony LED 40 inch",
+      variations: [
+        { id: 1, color: "black", price: 50000, quantity: 5 },
+        { id: 2, color: "red", price: 50000, quantity: 1 },
+        { id: 3, color: "silver", price: 55000, quantity: 8 },
+      ],
+      reviews: [
+        {
+          id: 1,
+          user: "Ahmad",
+          rating: 4.0,
+          title: "Good Product",
+          comments: "It is a very good product ....",
+          date: "06-02-2021",
+          status: true,
+        },
+        {
+          id: 2,
+          user: "Zubair",
+          rating: 4.5,
+          title: "Very Good Product",
+          comments: "zubair It is a very good product ....",
+          date: "05-02-2021",
+          status: false,
+        },
+        {
+          id: 3,
+          user: "Ali",
+          rating: 5.0,
+          title: "bad Product",
+          comments: "ali It is a very good product ....",
+          date: "04-02-2021",
+          status: true,
+        },
+      ],
+    },
+    {
+      id: 102,
+      title: "Mobile",
+      variations:[
+        { id: 1, color: "black", price: 50000, quantity: 5 },
+        { id: 2, color: "red", price: 50000, quantity: 1 },
+        { id: 3, color: "silver", price: 55000, quantity: 8 },
+      ],
+      reviews: [
+        {
+          id: 1,
+          user: "Ahmad",
+          rating: 4.0,
+          title: "Good Product",
+          comments: "It is a very good product ....",
+          date: "06-02-2021",
+          status: true,
+        },
+        {
+          id: 2,
+          user: "Zubair",
+          rating: 4.5,
+          title: "Very Good Product",
+          comments: "zubair It is a very good product ....",
+          date: "05-02-2021",
+          status: false,
+        },
+        {
+          id: 3,
+          user: "Ali",
+          rating: 5.0,
+          title: "bad Product",
+          comments: "ali It is a very good product ....",
+          date: "04-02-2021",
+          status: true,
+        },
+      ],
+    },
+    {
+      id: 103,
+      title: "Bike",
+      variations: [
+        { id: 1, color: "black", price: 55000, quantity: 5 },
+        { id: 2, color: "red", price: 50000, quantity: 1 },
+      ],
+      reviews: [
+        {
+          id: 1,
+          user: "Ahmad",
+          rating: 4.0,
+          title: "Good Product",
+          comments: "It is a very good product ....",
+          date: "06-02-2021",
+          status: true,
+        },
+        {
+          id: 2,
+          user: "Zubair",
+          rating: 3.0,
+          title: "Very Good Product",
+          comments: "zubair It is a very good product ....",
+          date: "05-02-2021",
+          status: false,
+        },
+      ],
+    },
+  ];
+  ////1: Use map to get an array of product titles
+
+  let productTitle = products.map( p => p.title);
+    console.log(productTitle);
+
+///////////
+
+
+///2: Use filter to get all products that have variations in black color
+
+let filterProduct = products.filter(f => {
+ f.variations.color === "black";
+});
+console.log(filterProduct);
+
+///
